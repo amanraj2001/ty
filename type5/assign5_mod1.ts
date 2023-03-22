d4341c24dd437c70d74bf2870f1e3af634b1cb97
amanraj2001 <123233930+amanraj2001@users.noreply.github.com>
2023-03-22 22:29:36 +0530

    Add files via upload

---
+++ type5/assign5_mod1.ts
@@ -0,0 +1,42 @@
+var arr = [
+    { "id": 0, "name": "aman", "city": "mumbai", "DOB": "02-02-2022" },
+    { "id": 1, "name": "aman2", "city": "ahmedabad", "DOB": "12-12-2002" },
+    { "id": 2, "name": "aman3", "city": "ahmedabad", "DOB": "12-12-2023" },
+    { "id": 2, "name": "aman3", "city": "ahmedabad", "DOB": "12-12-2025" },
+    { "id": 2, "name": "aman3", "city": "mumbai", "DOB": "12-12-2019" },
+]
+// console.log(arr[0].DOB)
+// var date = new Date(arr[0].DOB)
+
+arr.map((val, index) => {
+
+    let x = arr[index].DOB
+    // console.log(x)
+    var date = new Date(x);
+    var z = date.getFullYear()
+
+    if(z>2020){
+        // console.log("x")
+        // console.log()
+        console.log(arr[index])
+
+
+    }
+    // console.log(x) 
+    // console.log(val)
+})
+
+arr.map((val,index)=>{
+    let x = val.DOB
+    // console.log(x)
+    let date = new Date(x);
+    let y = val.city
+    // console.log(y)
+
+    if(date.getFullYear()>2020 && y=="mumbai"){
+        console.log(val)
+    }
+  
+
+})
+
